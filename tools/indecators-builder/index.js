3394866c45201c16ab899466312a8ca3abf375bf
Mtdung290504 <mtdung114290504@gmail.com>
2025-09-13 23:08:56 +0700

    13/9/2025 - 11:08 PM - Testing area aim indecators

--- tools/indecators-builder/index.js
+++ tools/indecators-builder/index.js
@@ -1,87 +1,17 @@
-// System builder and context
-import defineSystemFactory from '../../factory/factory_builders/defineSystemFactory.js';
-import { RenderContext } from './contexts.js';
+/**@type {HTMLCanvasElement} */
+const canvas = document.getElementById('aimCanvas');
+const ctx = canvas.getContext('2d');
+const { width, height } = canvas.getBoundingClientRect();
+if (!ctx) throw new Error();
 
-// Utils
-import { degToRad } from '../../fomulars/angle.js';
+canvas.width = width * 2;
+canvas.height = height * 2;
 
-// Components
-import MovementComponent from '../../components/combat/stats/com.Movement.js';
-import SpriteComponent from '../../components/graphic/com.Sprite.js';
-import PositionComponent from '../../components/physics/com.Position.js';
-import ShadowComponent from '../../components/graphic/com.Shadow.js';
-import InputComponent from '../../components/input/com.Input.js';
+const cx = canvas.width / 2;
+const cy = canvas.height / 2;
 
-const SpriteRenderer = defineSystemFactory([SpriteComponent, PositionComponent], RenderContext)
-	.withProcessor((context, eID, [sprite, pos], sysContext) => {
-		sysContext.addRenderCallback({ layer: sprite.getLayer(), render });
+renderAim(ctx, 300, { x: cx, y: cy });
 
-		function render() {
-			const { context2D } = sysContext;
-
-			// Sử dụng movement component nếu sprite cần xoay, không thì không đặt
-			const movementComponent = context.getComponent(eID, MovementComponent, false);
-			const angle = movementComponent?.angle ?? 0;
-
-			// Sử dụng cached frame data
-			const frameData = sprite.getCurrentFrameData();
-
-			// Sử dụng precomputed destination coordinates
-			const destCoords = sprite.getDestinationCoords(pos);
-			const { width: renderW, height: renderH } = destCoords.renderSize;
-
-			context2D.save();
-
-			// Nếu góc quay khác 0 mới xử lý xoay
-			if (angle !== 0) {
-				context2D.translate(pos.x, pos.y);
-				context2D.rotate(degToRad(angle));
-				context2D.translate(-pos.x, -pos.y);
-			}
-
-			// Render shadow nếu là tank
-			const shadow = context.getComponent(eID, ShadowComponent, false);
-			if (shadow) {
-				const { alpha, blur, offsetRatioX, offsetRatioY } = shadow;
-				context2D.shadowColor = `rgba(0, 0, 0, ${alpha})`;
-				context2D.shadowBlur = blur;
-				context2D.shadowOffsetX = renderW * offsetRatioX;
-				context2D.shadowOffsetY = renderH * offsetRatioY;
-			}
-
-			context2D.drawImage(
-				sprite.resource.sprite,
-				frameData.sx, // Tọa độ X gốc (trên ảnh nguồn) nơi bắt đầu cắt.
-				frameData.sy, // Tọa độ Y gốc (trên ảnh nguồn) nơi bắt đầu cắt.
-				frameData.sw, // Chiều rộng phần ảnh nguồn cần cắt.
-				frameData.sh, // Chiều cao phần ảnh nguồn cần cắt.
-				destCoords.dx, // Tọa độ X trên canvas nơi sẽ vẽ phần ảnh đã cắt.
-				destCoords.dy, // Tọa độ Y trên canvas nơi sẽ vẽ phần ảnh đã cắt.
-				destCoords.dw, // Chiều rộng để vẽ lên canvas. Nếu khác sw, ảnh sẽ bị co giãn theo tỷ lệ ngang.
-				destCoords.dh // Chiều cao để vẽ lên canvas. Nếu khác sh, ảnh sẽ bị co giãn theo tỷ lệ dọc.
-			);
-
-			// Vẽ border nếu trong mode debug
-			if (sysContext.getDebugState()) {
-				context2D.strokeStyle = 'white';
-				context2D.lineWidth = 2;
-				context2D.strokeRect(destCoords.dx, destCoords.dy, destCoords.dw, destCoords.dh);
-			}
-
-			context2D.restore();
-
-			const input = context.getComponent(eID, InputComponent, false);
-			if (!input) return;
-			context2D.save();
-			renderAim(context2D, 220, input.inputManager.mouseState);
-			context2D.restore();
-		}
-	})
-	.build();
-
-export default SpriteRenderer;
-
-// Test:
 /**
  * @param {CanvasRenderingContext2D} ctx
  * @param {number} radius
